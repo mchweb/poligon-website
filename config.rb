158c6e05b31c6f230544157385f15343953b9764
Mihail Kudryashev <uamedwed@gmail.com>
2016-12-13 02:03:25 +0300

    Addштп styles for header block.

--- config.rb
+++ config.rb
@@ -1,5 +1,11 @@
+require 'compass/import-once/activate'
+
 http_path = "/"
 css_dir = "assets/css"
 sass_dir = "scss"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
+
+output_style = :expanded
+
+line_comments = false
