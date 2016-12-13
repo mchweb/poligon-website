b0e0d45d414fe8b46b732c8be587ba76fde14003
Mihail Kudryashev <uamedwed@gmail.com>
2016-12-13 13:02:20 +0300

    Add styles and js for content-slider.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,31 @@
 /*==================   Basic    ======================*/
-/*==================   Home page    ======================*/
+/*==================   Functions    ==================*/
+var backgroundImageBlocks = function (blockName,imgName) {
+    var block =  document.querySelectorAll(blockName);
+    for (var i = 0; i < block.length; i++) {
+        var blockThumbnail = block[i].querySelector(imgName);
+        blockThumbnail.style.display = 'none';
+        block[i].style.backgroundImage = 'url('+blockThumbnail.getAttribute('src')+')';
+        block[i].style.backgroundPosition = 'right center';
+        block[i].style.backgroundRepeat = 'no-repeat';
+        block[i].style.backgroundSize = 'cover';   
+    }   
+};
+/*==================   Home page    ==================*/
 /*-------------  Infoblock  -------------*/
-var cInfoblocks =  document.getElementsByClassName('c-infoblock');
-for (var i = 0; i < cInfoblocks.length; i++) {
-    var cInfoblocksThumbnail = cInfoblocks[i].querySelector('.c-infoblock__thumbnail');
-    cInfoblocksThumbnail.style.display = 'none';
-    cInfoblocks[i].style.backgroundImage = 'url('+cInfoblocksThumbnail.getAttribute('src')+')';
-    cInfoblocks[i].style.backgroundPosition = 'right center';
-    cInfoblocks[i].style.backgroundRepeat = 'no-repeat';
-    cInfoblocks[i].style.backgroundSize = 'cover';
-    
-}
-//$(element).find('.c-topnews__thumbnail').find('img').css("display", "none");
-//            $(element).find('.c-topnews__background').css('background-image', 'url(' + $(element).find('.c-topnews__thumbnail').find('img').attr('src') + ')'); 
-//            $(element).find('.c-topnews__background').css({'background-position':'center center'});
-//            $(element).find('.c-topnews__background').css({'background-repeat':'no-repeat'});
-//            $(element).find('.c-topnews__background').css({'background-size': 'cover'}); 
-/*==================   Images    ======================*/    
-/*==================   Navigation menu    ======================*/
-/*==================   Animations   ======================*/
+backgroundImageBlocks('.c-infoblock','.c-infoblock__thumbnail');
+
+/*-------------  Slider  ----------------*/
+backgroundImageBlocks('.c-slider__item','.c-slider__thumbnail');
+backgroundImageBlocks('.c-slider-nav__item','.c-slider-nav__thumbnail');
+
+var elem = document.querySelector('.c-slider');
+var flkty = new Flickity();
+
+/*==================   Images    =====================*/    
+/*==================   Navigation menu    ============*/
+/*==================   Animations   ==================*/
+
 
 
 
