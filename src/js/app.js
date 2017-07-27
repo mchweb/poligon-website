384f62562b160ec6fcd7f3306258bcc62fdc9134
Mihail Kudryashev <uamedwed@gmail.com>
2017-07-27 14:57:04 +0300

    Add field and style with select file for form in the Elements page

--- src/js/app.js
+++ src/js/app.js
@@ -745,6 +745,42 @@ for (var i = 0; i < formButon.length; i++) {
 }  
 /* Set mask for phone input */    
 $('input[type="tel"]').inputmask({"mask": "+9 (999) 999-99-99"});
+/* Input file */    
+/*
+	By Osvaldas Valutis, www.osvaldas.info
+	Available for use under the MIT License
+*/
+
+'use strict';
+
+;( function ( document, window, index )
+{
+	var inputs = document.querySelectorAll( '.c-form__file' );
+	Array.prototype.forEach.call( inputs, function( input )
+	{
+		var label	 = input.nextElementSibling,
+			labelVal = label.innerHTML;
+
+		input.addEventListener( 'change', function( e )
+		{
+			var fileName = '';
+			if( this.files && this.files.length > 1 )
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			else
+				fileName = e.target.value.split( '\\' ).pop();
+
+			if( fileName )
+				label.querySelector( 'span' ).innerHTML = fileName;
+			else
+				label.innerHTML = labelVal;
+		});
+
+		// Firefox bug fix
+		input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+		input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+	});
+}( document, window, 0 ));
+
 /* Modal */
 /* Set a background image for modals */
 var modalFuncInit = function (eventResize) {
